be0f032e28abb4521afa8c3948b378c872ff5c0f
mcp48 <michael.c.payne83@gmail.com>
2021-07-29 17:25:24 -0400

    Fix: Deleted configuration info in app.py

--- app/app.py
+++ app/app.py
@@ -14,14 +14,5 @@ def create_app():
         return app
 
 
-mysql = MySQL(cursorclass=DictCursor)
-
-app.config['MYSQL_DATABASE_HOST'] = 'db'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_DB'] = 'mlbPlayerData'
-mysql.init_app(app)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
