688ce788554c5679e37906cd9b12417eee23a936
mcp48 <michael.c.payne83@gmail.com>
2021-07-29 17:24:00 -0400

    Feature: Updated app.py

--- app/app.py
+++ app/app.py
@@ -4,13 +4,15 @@ from flask import render_template
 from flaskext.mysql import MySQL
 from pymysql.cursors import DictCursor
 
-def create_app()
+
+def create_app():
     app = Flask(__name__, template_folder="templates")
 
     with app.app_context():
         from . import routes
 
         return app
+
 
 mysql = MySQL(cursorclass=DictCursor)
 
@@ -20,8 +22,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_DB'] = 'mlbPlayerData'
 mysql.init_app(app)
-
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
