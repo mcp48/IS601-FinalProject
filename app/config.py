bf5bf7fdf6f584dd06e8cc84c6a5738a5c837aa4
mcp48 <michael.c.payne83@gmail.com>
2021-08-01 14:21:32 -0400

    Feature: Added static folder and templates folder to config.py

--- app/config.py
+++ app/config.py
@@ -9,3 +9,6 @@ class Config:
     SECRET_KEY = environ.get("SECRET_KEY")
     FLASK_APP = environ.get("FLASK_APP")
     FLASK_ENV = environ.get("FLASK_ENV")
+
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
