aa9147d220d927638bbc6ea2c362bf1853fa7770
mcp48 <michael.c.payne83@gmail.com>
2021-08-01 14:48:22 -0400

    Feature: Created login and authentication files

--- app/forms.py
+++ app/forms.py
@@ -29,3 +29,16 @@ class SignupForm(FlaskForm):
         ]
     )
     submit = SubmitField("Submit")
+
+
+class LoginForm(FlaskForm):
+    """User Log-in Form."""
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(message='Enter a valid email.')
+        ]
+    )
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
