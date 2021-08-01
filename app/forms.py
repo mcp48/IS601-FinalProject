bf9192aedff93de851b2dcf295f38b52c4138adb
mcp48 <michael.c.payne83@gmail.com>
2021-08-01 15:34:47 -0400

    Fix: imported Length from wtforms.validators on forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, EqualTo, Email, Optional
+from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional
 
 
 class SignupForm(FlaskForm):
